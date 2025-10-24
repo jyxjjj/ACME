de881d81b7ba498e46659fa90076fe0c453a0421
jyxjjj <773933146@qq.com>
2025-10-24 14:27:04 +0800

    Update auth.go

--- src/auth.go
+++ src/auth.go
@@ -8,6 +8,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// Validate essential auth-related env vars at startup so failures surface early
+	orgName := os.Getenv("CF_ZT_ORG_NAME")
+	policyAUD := os.Getenv("CF_ZT_AUD")
+	if orgName == "" || policyAUD == "" {
+		Log.Fatalln("CF_ZT_ORG_NAME or CF_ZT_AUD is not set; auth cannot function")
+	}
+}
+
 func authVerify(c *gin.Context) {
 	token := c.GetHeader("Cf-Access-Jwt-Assertion")
 	if token == "" {
@@ -26,9 +35,10 @@ func authVerify(c *gin.Context) {
 	config := &oidc.Config{
 		ClientID: policyAUD,
 	}
-	keySet := oidc.NewRemoteKeySet(c, certsURL)
+	ctx := c.Request.Context()
+	keySet := oidc.NewRemoteKeySet(ctx, certsURL)
 	verifier := oidc.NewVerifier("https://"+orgName+".cloudflareaccess.com", keySet, config)
-	_, err := verifier.Verify(c, token)
+	_, err := verifier.Verify(ctx, token)
 	if err != nil {
 		jsonResponse(c, ErrUnauthorized, nil)
 		c.Abort()
